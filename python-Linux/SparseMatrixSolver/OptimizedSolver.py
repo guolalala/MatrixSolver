622db126ba0f6e68e9565c0283319b0d13eb5039
sy2221102 <sy2221102@buaa.edu.cn>
2024-03-26 20:20:23 +0800

    update

--- python-Linux/SparseMatrixSolver/OptimizedSolver.py
+++ python-Linux/SparseMatrixSolver/OptimizedSolver.py
@@ -4,17 +4,17 @@ from ctypes import *
 # Load the shared library
 module_path = os.path.dirname(__file__)
 dll_path = os.path.join(module_path, "libSolver.so")
-nicslu=CDLL(dll_path)
+optisolver=CDLL(dll_path)
 
 # Call the main_function
-def NICSLUSolve(A,B=None,X=None):
+def Solve(A,B=None,X=None):
     if A!=None:
         A = A.encode()
     if B!=None:
         B = B.encode()
     if X!=None:
         X = X.encode()
-    nicslu.solve(A,B,X)
+    optisolver.solve(A,B,X)
 
 if __name__ == '__main__':
-    NICSLUSolve("../datasets/ACTIVSg10K.mtx", "../datasets/ACTIVSg10K_b.mtx", "../NICSLU.log")
+    Solve("../datasets/ACTIVSg10K.mtx", "../datasets/ACTIVSg10K_b.mtx", "../NICSLU.log")
--- python-Linux/test.py
+++ python-Linux/test.py
@@ -1,3 +1,7 @@
 from SparseMatrixSolver import nicslu
+from SparseMatrixSolver import eigen
+from SparseMatrixSolver import OptimizedSolver
 
-nicslu.NICSLUSolve("./datasets/ACTIVSg10K.mtx")
+# nicslu.NICSLUSolve("./datasets/ACTIVSg10K.mtx")
+# eigen.LLTSolve("./datasets/ACTIVSg10K.mtx")
+OptimizedSolver.Solve("./datasets/ACTIVSg10K.mtx")
